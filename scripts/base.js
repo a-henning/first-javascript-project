2335f7b823a5106ca41117147139cfdbf05cd2c2
Arthur Henning <h_arthur_12@yahoo.com>
2014-02-18 11:15:49 +0200

    Added dropdown and login form.

--- scripts/base.js
+++ scripts/base.js
@@ -5,7 +5,49 @@ $(document).ready(function() {
 		width: 940,
 		height: 528,
 		navigation: {
-			effect: "fade"
+			effect: "slide"
 		}
 	});
+
+	//Dropdown plugin data
+	var ddData = [
+	    {
+	        text: "Facebook",
+	        value: 1,
+	        selected: false,
+	        description: "Description with Facebook",
+	        imageSrc: "http://dl.dropbox.com/u/40036711/Images/facebook-icon-32.png"
+	    },
+	    {
+	        text: "Twitter",
+	        value: 2,
+	        selected: false,
+	        description: "Description with Twitter",
+	        imageSrc: "http://dl.dropbox.com/u/40036711/Images/twitter-icon-32.png"
+	    },
+	    {
+	        text: "LinkedIn",
+	        value: 3,
+	        selected: false,
+	        description: "Description with LinkedIn",
+	        imageSrc: "http://dl.dropbox.com/u/40036711/Images/linkedin-icon-32.png"
+	    },
+	    {
+	        text: "Foursquare",
+	        value: 4,
+	        selected: false,
+	        description: "Description with Foursquare",
+	        imageSrc: "http://dl.dropbox.com/u/40036711/Images/foursquare-icon-32.png"
+	    }
+	];
+
+	$('.dropDown').ddslick({
+    data: ddData,
+    width: 399,
+    imagePosition: "left",
+    selectText: "Login with social network",
+    onSelected: function (data) {
+        console.log(data);
+    }
+});
 });
